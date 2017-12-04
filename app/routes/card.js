ed095ebd83d1eecdff67b4f38956c58b71baac6a
Manuel Hernandez <dimasi.developer@gmail.com>
2017-12-04 12:47:42 -0400

    finished the aspect of the game

--- app/routes/card.js
+++ app/routes/card.js
@@ -3,5 +3,11 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(params) {
     return params.play_id;
+  },
+  afterModel(){
+    /*Ember.run.later((() => {
+      Ember.$('#carta').removeClass("cara2");
+      console.log("removeClass()");
+    }), 8000);*/
   }
 });
